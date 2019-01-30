b04a9e639ebc12bc5c9bf33b9c085af51f2f0c89
Christina Wolff <cwolff@juggernaut.highpoint.edu>
2019-01-29 20:50:56 -0500

    Final so far

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -11,7 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
- 
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+
     let questions: [String] = [
         "What is 7+7",
         "What is the capital of Vermont?",
@@ -39,10 +45,6 @@ class ViewController: UIViewController {
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
-    }
-
 }
+
 
